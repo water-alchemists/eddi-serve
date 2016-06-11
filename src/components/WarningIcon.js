e7fb274e952bd74f7017786f45bf20e503d7e579
Tony <terimyaki@users.noreply.github.com>
2016-06-11 13:53:39 -0400

    added warning component

--- src/components/WarningIcon.js
+++ src/components/WarningIcon.js
@@ -3,8 +3,9 @@ import React, { Component, PropTypes } from 'react';
 import classNames from 'classnames';
 
 class WarningIcon extends Component {
-
     render(){
+        const { show } = this.props;
+        if(!show) return null;
         return (
             <div className='warning-icon sprite circle'>
                 <p>!</p>
@@ -14,7 +15,11 @@ class WarningIcon extends Component {
 }
 
 WarningIcon.propTypes = {
+    show : PropTypes.bool.isRequired
+};
 
+WarningIcon.defaultProps = {
+    show : true
 };
 
 export default WarningIcon;
