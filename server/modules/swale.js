896ef702d8f0bf6bc600109da9904131dca65645
Tony <terimyaki@users.noreply.github.com>
2016-10-14 00:24:44 -0400

    added demo script for getting from swale api

--- server/modules/swale.js
+++ server/modules/swale.js
@@ -1,16 +1,23 @@
 'use strict';
-import request from 'superagent';
+const request = require('superagent');
 const API_ROUTE = 'http://54.235.200.47/water'
 
 function post(data){
-    return request.post(API_ROUTE)
+    return new Promise((resolve, reject) => {
+        request.post(API_ROUTE)
             .send(data)
-            .set('Accept', 'application/json');
+            .set('Accept', 'application/json')
+            .then(resolve, reject);
+    });
 }
 
 function get(data){
-    return request.get(`${API_ROUTE}/*`)
-            .set('Accept', 'application/json');
+    return new Promise((resolve, reject) => {
+        request.get(`${API_ROUTE}/*`)
+            .set('Accept', 'application/json')
+            .then(resolve, reject);
+    })
+    .then(res => res.body);
 }
 
 module.exports = {
